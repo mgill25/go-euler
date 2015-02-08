f3252dd63f000b6f12451dc646c4382184bb7570
Manish Gill <mgill25@outlook.com>
2015-02-08 18:43:14 +0530

    Faster non brute-force version, using prime sieve and counting divisors via prime factorization

--- p12.go
+++ p12.go
@@ -10,77 +10,88 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"time"
 )
 
-func isPrime(n int) bool {
-	if n == 0 || n == 1 {
-		return false
+func Primes(n int) []int {
+	// Generate Prime numbers upto n using Sieve of Eratosthenes.
+	sieve := make([]bool, n)
+	for i := 0; i < n; i++ {
+		sieve[i] = true
 	}
-	var i int
-	var limit = int(math.Sqrt(float64(n)))
-	for i = 2; i <= limit; i += 1 {
-		if n%i == 0 {
-			return false
+
+	// now cross off the non-primes.
+	primes := make([]int, 0)
+	for i := 2; i < n; i++ {
+		if sieve[i] {
+			primes = append(primes, i)
+			for j := i * i; j < n; j += i {
+				sieve[j] = false
+			}
 		}
 	}
-	return true
+	return primes
 }
 
-func primeFactorize(n int, acc []int) []int {
-	// Recursively accumulate all the prime factors
-	var i int = 2
-	if n/i == 0 {
-		return acc
-	}
-	for i <= n {
-		if n/i == 0 {
-			acc = append(acc, i)
-			return acc
+func countDivisors(n int, primes []int, divisors int) int {
+	// helper function
+	// keep reducing the number until it becomes 1
+	// Iterate over each prime number and count its exponent as it is able
+	// to divide the remaining quotient. Add one to each prime's exponent
+	// and multiply them at the end.
+	// Ref: http://www.wikihow.com/Determine-the-Number-of-Divisors-of-an-Integer
+
+	k := 0       // prime index
+	for n != 1 { // keep reducing the number until it becomes 1
+		count := 1
+		for n%primes[k] == 0 {
+			n /= primes[k]
+			count++
 		}
-		if isPrime(i) && n%i == 0 {
-			acc = append(acc, i)
-			return primeFactorize(n/i, acc)
-		} else {
-			i += 1
-		}
+		divisors *= count
+		k++
 	}
-	return acc
+	return divisors
 }
 
-func countFactors(n int) int {
-	// Count all factors using just the prime factors!
-	factors := primeFactorize(n, make([]int, 0))
-	midmap := make(map[int]int)
-	for _, j := range factors {
-		_, present := midmap[j]
-		if !present {
-			midmap[j] = 1
-		} else {
-			midmap[j] += 1
+func countFactors(n int, primes []int) int {
+	// Every prime number only has 2 factors: Itself and 1.
+	for i := 0; i < len(primes); i++ {
+		if primes[i] == n {
+			return 2
 		}
 	}
 
-	var rv int = 1
-	for _, v := range midmap {
-		rv *= (v + 1)
-	}
-	return rv
+	// if non-prime, we use formula!
+	return countDivisors(n, primes, 1)
+}
+
+func triangular(n int) int {
+	return n * (n + 1) / 2
 }
 
 func main() {
-	found := false
+	t0 := time.Now()
+
 	num := 1
-	count := 2
-	for !found {
-		// fmt.Println(num, ": ", countFactors(num))
-		if countFactors(num) > 500 {
-			fmt.Println("Found: ", num)
-			found = true
-		} else {
-			// Get the next triangle number in the series.
-			num += count
-			count = count + 1
-		}
+
+	// generate primes via Sieve of Eratosthenes
+	primeLen := 100000
+	primes := Primes(primeLen)
+
+	triangular_number := triangular(num)
+	num_divisors := countFactors(triangular_number, primes)
+
+	for num_divisors <= 500 {
+		// fmt.Println(triangular_num, ": ", num_divisors)
+		num += 1
+		triangular_number = triangular(num)
+		num_divisors = countFactors(triangular_number, primes)
 	}
+
+	fmt.Println("Found: ", triangular_number, " with ", num_divisors, " divisors")
+	t1 := time.Now()
+	fmt.Println(t1.Sub(t0))
+
+	// fmt.Println("Divisors of 16: ", countDivisors(16, primes, 1)) 	// 2^4 * 1^1
 }
